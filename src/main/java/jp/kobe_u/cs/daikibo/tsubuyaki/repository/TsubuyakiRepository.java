207469e0d5784897c9c87718f55f10990968a027
WatanabeKeita1875040T <1875040t@stu.kobe-u.ac.jp>
2022-07-28 17:29:04 +0900

    complete local tsubuyaki

--- src/main/java/jp/kobe_u/cs/daikibo/tsubuyaki/repository/TsubuyakiRepository.java
+++ src/main/java/jp/kobe_u/cs/daikibo/tsubuyaki/repository/TsubuyakiRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 import jp.kobe_u.cs.daikibo.tsubuyaki.entity.Tsubuyaki;
 
 @Repository
-  
 public interface TsubuyakiRepository extends CrudRepository<Tsubuyaki, Long>
 {
     List<Tsubuyaki> findByCommentContaining(String keyword);
